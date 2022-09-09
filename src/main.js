cd7ebe3c364f1ed4bdc5e03179198d2220bfea8e
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-09-09 14:34:07 -0500

    parte firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-// Este es el punto de entrada de tu aplicacion
+//Mostrar nuestro HTML "Home"
+//Mostrar la pag de RegisterNumber
+//Mostrar la pag de Login. 
 
-import { myFunction } from './lib/index.js';
+const rootDiv= document.getElementById("root");
 
-myFunction();
+window.addEventListener('hashchange',() => {
+    if(window.location.hash === "#registerPhone"){
+    console.log("view page of register phone");
+    rootDiv.innerHTML= "<h1> Register of phone number </h1>" ;
+
+    }if(window.location.hash === "#Login"){
+        console.log("view Login");
+        rootDiv.innerHTML= "<h1> Sing In </h1>";
+    }
+});
